6d72b00355b8744466a905f0a4a360aded66af5a
Samuel <samuel.hilty@students.fhnw.ch>
2022-04-02 11:43:04 +0200

    ClassPriceCalc
    Preiskalkulation + Test

---
+++ src/main/java/ch/fhnw/lems/business/PriceCalculation.java
@@ -0,0 +1,14 @@
+package ch.fhnw.lems.business;
+
+public class PriceCalculation {
+
+    //Preiskalulation EXPRESS
+
+    //Formel: x=Distanz, y=Anz.Pal, a=Faktor, b=GewichtungPal, c=Zusatzgebühr
+    // Preis = x*a + y^b * a + c
+
+
+
+
+
+}
