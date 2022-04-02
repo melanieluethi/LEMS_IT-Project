5a542bd09486a015a2e736b0a7a67ca4bcd41d51
Samuel <samuel.hilty@students.fhnw.ch>
2022-04-02 15:02:45 +0200

    PC
    PC v0.00001

---
+++ src/main/java/ch/fhnw/lems/business/PriceCalculationPackage.java
@@ -0,0 +1,11 @@
+package ch.fhnw.lems.business;
+
+public class PriceCalculationPackage {
+
+    //if size & weight smaller than x Pallets & x KG --> offer Package price.
+    //if not applicaple return "not applicable"
+
+
+
+
+}
