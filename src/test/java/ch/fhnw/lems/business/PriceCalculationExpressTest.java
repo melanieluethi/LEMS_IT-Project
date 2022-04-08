8670c7df470fd20d699d3b2cde3bc6427a298d9f
Samuel <samuel.hilty@students.fhnw.ch>
2022-04-08 21:53:45 +0200

    [HIS] Empty Junit Tests added

---
+++ src/test/java/ch/fhnw/lems/business/PriceCalculationExpressTest.java
@@ -0,0 +1,17 @@
+package ch.fhnw.lems.business;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+//work @HIS
+
+class PriceCalculationExpressTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
