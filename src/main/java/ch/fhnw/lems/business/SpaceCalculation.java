d1846b67433a538996ccf873a0e754c2210d8da7
Samuel <samuel.hilty@students.fhnw.ch>
2022-04-09 11:35:02 +0200

    [HIS] palett space calc

--- src/main/java/ch/fhnw/lems/business/SpaceCalculation.java
+++ src/main/java/ch/fhnw/lems/business/SpaceCalculation.java
@@ -1,20 +1,76 @@
 package ch.fhnw.lems.business;
 
-
 //done by HiS
 public class SpaceCalculation {
 
+	// Paletten-Platzbedarf wird IMMER Aufgerundet
 
+	// max 300kg pro Palette
+	// P1 max Stack:25 Min Space:1.2 weight: 25pc = 500kg --> 20kg
+	// P2 max Stack:10 Min Space:2 weight: 10pc = 600kg --> 60kg
+	// P3 max Stack:15 Min Space:2.5 weight 15pc = 870kg --> 58kg
+	// P4 max Stack:100 Min Space:0.8 weight: 100pc = 200kg --> 2kg
 
-    //Paletten-Platzbedarf wird IMMER Aufgerundet
+	private int testSumP1;
+	private int testSumP2;
+	private int testSumP3;
+	private int testSumP4;
 
-    //max 300kg pro Palette
-    //P1 max Stack:25 Min Space:1.2
-    //P2 max Stack:10 Min Space:2
-    //P3 max Stack:15 Min Space:2.5
-    //P4 max Stack:100 Min Space:0.8
+	private int weightMultiplicatorP1 = 20;
+	private int weightMultiplicatorP2 = 60;
+	private int weightMultiplicatorP3 = 58;
+	private int weightMultiplicatorP4 = 2;
 
+	private int weightP1;
+	private int weightP2;
+	private int weightP3;
+	private int weightP4;
 
+	private int totalWeight;
+	
+	private double palletSpaceP1;
+	private double palletSpaceP2;
+	private double palletSpaceP3;
+	private double palletSpaceP4;
 
+	//calculate the weight per Product
+	public double calculateWight() {
+
+		if (testSumP1 != 0) {
+			weightP1 = testSumP1 * weightMultiplicatorP1;
+		}
+
+		if (testSumP2 != 0) {
+			weightP2 = testSumP2 * weightMultiplicatorP2;
+		}
+
+		if (testSumP3 != 0) {
+			weightP3 = testSumP3 * weightMultiplicatorP3;
+		}
+
+		if (testSumP4 != 0) {
+			weightP4 = testSumP4 * weightMultiplicatorP4;
+		}
+
+		totalWeight = weightP1 + weightP2 + weightP3 + weightP4;
+
+		return 0;
+
+	}
+
+	//Calculate the needed space
+	// max weight = 300kg/palett
+	public double calculateSpace() {
+
+		if (testSumP1 != 0) {
+			palletSpaceP1 = 0.0;
+		}
+		
+		
+		
+		
+		
+		return 0;
+	}
 
 }
---
+++ src/test/java/ch/fhnw/lems/business/SpaceCalculationTest.java
@@ -0,0 +1,14 @@
+package ch.fhnw.lems.business;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SpaceCalculationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
