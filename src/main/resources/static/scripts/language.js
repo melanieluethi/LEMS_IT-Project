664a5e8ec61c21ffd0aed292c1cdd07d1c13302d
Eveline Walker <eveline.walker@gmx.ch>
2022-05-05 19:46:13 +0200

    [WAE] New Try Language

---
+++ src/main/resources/static/scripts/language.js
@@ -0,0 +1,20 @@
+// wae angelehnt an https://stackoverflow.com/questions/46008760/how-to-build-multiple-language-website-using-pure-html-js-jquery
+var language;
+function getLanguage(){
+	(localStorage.getItem('language')== null)? setLanguage('en'):false;
+	$ajax({
+		url:'/language/' + localStorage.getItem('language') + '.json',
+		dataType:'json', async:false, dataType:'json',
+		success:function(lang){language = lang}});
+	}
+	
+function setLanguage(){
+	localStorage.setItem('language', lang);
+}
+window.onload = function(){
+	getLanguage();
+}
+
+$(document).ready(function(){
+	$('#welcome'.text(language.date));
+});
