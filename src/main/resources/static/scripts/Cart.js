312a6cdd90f5a16d0f121c569ef0f46b013f12b0
Lea <leamaria.schmidli@students.fhnw.ch>
2022-05-08 18:09:10 +0200

    Cart and Product View

---
+++ src/main/resources/static/scripts/Cart.js
@@ -0,0 +1,18 @@
+// lum
+function getShipping() {
+	const titleFullName = document.getElementById('titleFullName');	
+	 $.ajax({
+        type: "GET",
+        url: "/api/user",
+        success: function (data) {
+			titleFullName.innerHTML = titleFullName.innerHTML.replace('fullname', data.firstname + ' ' + data.lastname);
+        }, error: function(e) {
+			console.log(e);
+	  	}
+	});
+}
+
+// lum
+window.onload = function() {
+  getUserFullName();
+};
--- src/main/resources/static/scripts/ProductChoice.js
+++ src/main/resources/static/scripts/ProductChoice.js
@@ -1,22 +1,15 @@
-//SCL
+// SCL
 function getProduct() {
-
-	var element = document.getElementById('products');
-    var name = element.options[element.selectedIndex].text;
-    var wert = element.options[element.selectedIndex].value;
-    alert('Wert: '+wert+' - Name: '+name);
+	
     
-    
-	//const titleFullName = document.getElementById('titleFullName');	
+	const productname = document.getElementById('products');	
 	 $.ajax({
-        type: "GET",
-        url: "/api/user",
-        success: function (data) {
-			titleFullName.innerHTML = titleFullName.innerHTML.replace('fullname', data.firstname + ' ' + data.lastname);
+		success: function (data) {
+        productname.innerHTML = productname.innerHTML.replace('pName');
         }, error: function(e) {
 			console.log(e);
 	  	}
-	});
+	  	});
 }
 
 // lum
