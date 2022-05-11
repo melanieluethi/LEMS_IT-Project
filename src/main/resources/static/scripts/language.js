be38d44117a3efcf61fa7c4ca829f1a3162f7c4b
Melanie Lüthi <melanieluethi1167@gmail.com>
2022-05-11 17:04:04 +0200

    [LUM] language

--- src/main/resources/static/scripts/language.js
+++ src/main/resources/static/scripts/language.js
@@ -1,19 +1,16 @@
 // wae angelehnt an https://stackoverflow.com/questions/46008760/how-to-build-multiple-language-website-using-pure-html-js-jquery
 function setLanguage(){
+	debugger;
 	localStorage.setItem('language', lang);
 }
 
-/*
 window.onload = function(){
-
-}
-*/
-
-$(document).ready(function() {
+debugger;
     $("#locales").change(function () {
+	debugger;
         var selectedOption = $('#locales').val();
         if (selectedOption != ''){
             window.location.replace('international?lang=' + selectedOption);
         }
     });
-});
+}
