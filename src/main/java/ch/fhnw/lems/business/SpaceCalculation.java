db0beb5710e4c6c6585be9d7a7c7d61d2eb31656
Samuel <samuel.hilty@students.fhnw.ch>
2022-05-16 22:20:48 +0200

    [HiS] Change to int

--- src/main/java/ch/fhnw/lems/business/SpaceCalculation.java
+++ src/main/java/ch/fhnw/lems/business/SpaceCalculation.java
@@ -214,7 +214,7 @@ public class SpaceCalculation {
 	// P4 max Stack:100 Min Space:0.8 weight: 100pc = 200kg --> 2kg
 
 	
-	public double totalPalletts(int sumP1, int sumP2, int sumP3, int sumP4) {
+	public int totalPalletts(int sumP1, int sumP2, int sumP3, int sumP4) {
 		testSumP1 = sumP1;
 		testSumP2 = sumP2;
 		testSumP3 = sumP3;
@@ -222,7 +222,9 @@ public class SpaceCalculation {
 		
 		calculateRestSpace();
 		useRestSpace();
-		double palletts = calculateSpace();
+		double pallett = calculateSpace();
+		int palletts = (int) pallett;
+		
 		
 		return palletts;
 	}
