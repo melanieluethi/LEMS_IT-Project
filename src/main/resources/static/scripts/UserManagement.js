45ddbd7cfeff4edfe34161eafd63507bbd207d98
Eveline Walker <eveline.walker@gmx.ch>
2022-05-23 19:26:34 +0200

    [WAE] UserManagement automatically, styling nav-bar

--- src/main/resources/static/scripts/UserManagement.js
+++ src/main/resources/static/scripts/UserManagement.js
@@ -1,15 +1,17 @@
 //Wae
-function getUserData(){
+function getUser() {
 	let baseUrl = window.location.href; 
 	let userId = baseUrl.substring(baseUrl.lastIndexOf('=') + 1);
 	let userIdField = document.getElementById('userId');
 	userIdField.value = userId;
 	$.ajax({
-		type:"GET",
+		type: "GET",
 		url: "/api/profileSettings/" + userId,	
 		dataType: 'json',
-		contentType: 'application/json',
-		success:function (data) {
+		contentType: 'application/json',	
+		success: function (data) {			
+			let userId = document.getElementById('userId');
+			userId.value = data.id;
 			let fname = document.getElementById('fname');
 			fname.value = data.firstname;
 			let lname = document.getElementById('lname');
@@ -27,16 +29,47 @@ function getUserData(){
 			let password = document.getElementById('password');
 			password.value = data.password;
 			let username = document.getElementById('username');
-			username.value = data.username;		
+			username.value = data.username;			
         }, 
         error: function(e) {
 			console.log(e);
 	  	}
-	})
+	});
 }
 
+function getLoggedInUser() {
+	$.ajax({
+		type: "GET",
+		url: "/api/user/",	
+		dataType: 'json',
+		contentType: 'application/json',	
+		success: function (data) {	
+			let userId = document.getElementById('userId');
+			userId.value = data.userId;
+			let fname = document.getElementById('fname');
+			fname.value = data.firstname;
+			let lname = document.getElementById('lname');
+			lname.value = data.lastname;
+			let address = document.getElementById('address');
+			address.value = data.address;
+			let zip = document.getElementById('zip');
+			zip.value = data.postalCode;
+			let city = document.getElementById('city');
+			city.value = data.city;
+			let country = document.getElementById('country');
+			country.value = data.country;
+			let mail = document.getElementById('mail');
+			mail.value = data.email;
+			let username = document.getElementById('username');
+			username.value = data.username;			
+        }, 
+        error: function(e) {
+			console.log(e);
+	  	}
+	});
+}
 
-function putUserData() {
+function save() {
 	let userId = document.getElementById('userId').value;
 	let fname = document.getElementById('fname').value;
 	let lname = document.getElementById('lname').value;
@@ -45,7 +78,7 @@ function putUserData() {
 	let city = document.getElementById('city').value;
 	let country = document.getElementById('country').value;
 	let mail = document.getElementById('mail').value;
-	let username = document.getElementById('username').value;
+	let username = document.getElementById('username').value;	
 	
 	$.ajax({
 		type: 'PUT',
@@ -61,13 +94,18 @@ function putUserData() {
 			country: country,
 			email: mail,
 			password: '',
+			role: role, 
 			language: ''									
 		}),	
 		dataType: 'json',
 		contentType: 'application/json',	
 		success: function (data) {
 			if(data) {
-				window.location.href='/adminUsers';	
+				if(isProfileData){
+					window.location.href='/userManagement';	
+				} else {
+					window.location.href='/users' + lang + '&id=' + userId;		
+				}
 			} else {
 				if(window.location.search.includes('eng')){
 					alert('Something went wrong.');
@@ -101,7 +139,11 @@ function changePw() {
 		contentType: 'application/json',	
 		success: function (data) {
 			if(data) {
-				window.location.href='/adminUsers';	
+				if(isProfileData){
+					window.location.href='/userManagement';	
+				} else {
+					window.location.href='/users' + lang + '&id=' + userId;		
+				}
 			} else {
 				if(window.location.search.includes('eng')){
 					alert('Something went wrong.');
@@ -121,6 +163,20 @@ function changePw() {
 	});
 }
 
+function isProfileData() {
+	let baseUrl = window.location.href; 
+	let searchAttr = baseUrl.substring(baseUrl.lastIndexOf('=') + 1);
+	if(searchAttr === 'de' || searchAttr === 'eng'){
+		return true;
+	} else {
+		return false;	
+	}
+}
+
 window.onload = function() {
-  getUserData();
+	if(isProfileData){
+		getLoggedInUser();
+	} else {
+		getUser();	
+	}
 };
